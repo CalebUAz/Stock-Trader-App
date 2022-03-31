d02fae8b7ac0580b26abff2fa2febf8fc3edea25
CalebUAz <calebshibu@email.arizona.edu>
2022-03-30 23:46:53 -0700

    Hash function and create stock functions

---
+++ StockTraderApp.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import yfinance as yf
+from yahoofinancials import YahooFinancials
+import streamlit as st
+from PIL import Image
+import requests
+import pandas_datareader as web
+import sqlite3 
+import hashlib
+from datetime import date, datetime
+import random
+from random import gauss
+
+conn = sqlite3.connect('data.db')
+c = conn.cursor()
+
+def make_hashes(password):
+    return hashlib.sha256(str.encode(password)).hexdigest()
+
+def check_hashes(password,hashed_text):
+    if make_hashes(password) == hashed_text:
+        return hashed_text
+    return False
+
+def create_stocks():
+    #Idea behind 
+    df = pd.DataFrame(columns=['Date','Open','Close','High','Low','AdjClose','Volume'])
+    df.Date = pd.date_range(start='03-01-2022', end=datetime.now().strftime('%Y-%m-%d, %H:%M:%S'), freq="1min")
+
+    for i in range(len(df)):
+        value = gauss(50, 100)
+        
+        if value < 0:
+            value = value * -1
+
+        df.Open[i] = value
+        df.Close[i] = value + gauss(5, 10)
+        df.High[i] = value + gauss(5, 10)
+        df.Low[i]  = value + gauss(5, 10)
+        df.AdjClose[i]  = value + gauss(5, 10)	
+        #df.Volume[i]  = value + gauss(5, 10)
+    st.dataframe(df)
+    return df    
